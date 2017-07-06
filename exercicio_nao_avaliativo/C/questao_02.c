aee99621dc4122b337915eb29e0e20cb38f49a14
Rogério de Lima Sousa <rogeriothe@gmail.com>
2017-07-06 19:37:12 -0300

    Q02 Exercicio OK

--- exercicio_nao_avaliativo/C/questao_02.c
+++ exercicio_nao_avaliativo/C/questao_02.c
@@ -1,11 +1,21 @@
+#include <conio.h>
 #include <stdio.h>
 
 int main(int argc, char** argv)
 {
-	char data[6];
+	int data, ano, mes, dia;
 	
-	gets(data);
-	printf("%c", data);
+	printf("Digite a data no formato ddmmaa\n");
+	scanf("%d", &data);
+	
+	dia = data / 10000;	
+	data = data % 10000;	
+	mes = data / 100;
+	ano = data % 100;
 		
+	printf("Dia: %d\n", dia);
+	printf("Mes: %d\n", mes);	
+	printf("Ano: %d\n", ano);
+	
 	return 0;
 }
