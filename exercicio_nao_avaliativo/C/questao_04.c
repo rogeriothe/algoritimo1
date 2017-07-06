fabaadc2aafff71e28803fe0a546b44c2c6613e2
Rogério de Lima Sousa <rogeriothe@gmail.com>
2017-07-06 19:58:21 -0300

    Q04 Exercicio OK

---
+++ exercicio_nao_avaliativo/C/questao_04.c
@@ -0,0 +1,26 @@
+#include <conio.h>
+#include <stdio.h>
+
+void main()
+{
+	float salario, valor_emprestimo, num_parcelas;
+	
+	printf("Qual o valor do salario?\n");
+	scanf("%f", &salario);
+
+	printf("Qual o valor do emprestimo?\n");
+	scanf("%f", &valor_emprestimo);	
+
+	printf("Qual a quantidade de parcelas?\n");
+	scanf("%f", &num_parcelas);
+
+	if((valor_emprestimo / num_parcelas) > (salario * 0.3))
+	{
+		printf("Emprestimo nao aprovado");
+	}
+	else
+	{
+		printf("Emprestimo aprovado");		
+	}
+
+}
