c0f3ac4d2633b50bd48ea9e610731c57fb98d5a3
Rogério de Lima Sousa <rogeriothe@gmail.com>
2017-07-12 09:08:56 -0300

    questoes em c ok

---
+++ exercicio_avaliativo/C/questao_10.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	float peso, altura, imc;
+	
+	printf("Digite o seu peso:\n");
+	scanf("%f", &peso);
+	
+	printf("Digite sua altura:\n");
+	scanf("%f", &altura);
+	
+	imc = (peso / (altura * altura));
+	
+	printf("Seu imc: %f\n", imc);
+	
+	
+	if(imc < 20.00)
+	{
+		printf("Voce esta abaixo do peso\n");		
+	}
+	else
+	{
+		if (imc >= 20.00 && imc < 25.00) 
+		{
+			printf("Voce esta com peso normal\n");		
+		}
+		else
+		{
+			if (imc > 25.00 && imc <= 30.00) 
+			{
+				printf("Voce esta com excesso de peso\n");		
+			}
+			else
+			{
+				if (imc > 30.00 && imc <= 35.00) 
+				{
+					printf("Voce esta com obesidade\n");		
+				}
+				else
+				{
+					if (imc > 35.00) 
+					{
+						printf("Voce esta com obesidade mobida\n");		
+					}					
+				}				
+			}
+		}
+		
+	}
+	
+	return 0;
+}
